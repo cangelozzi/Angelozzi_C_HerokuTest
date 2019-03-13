c8ffdbd6b93921cd165634d89c93cb820c4cf440
camgelozzi <cangelozzi@gmail.com>
2019-03-13 13:49:13 -0400

    userpage added

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+  name: "Camillo",
+  role: "Papa",
+
+  user1: {
+    name: "user1",
+    role: "developer",
+    bio: "bio",
+    img: "roger.png"
+  },
+  user2: {
+    name: "user2",
+    role: "developer",
+    bio: "bio",
+    img: "david.png"
+  }
+
+}
+
+module.exports = data;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+//!Require data.js
+const data = require('../data');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', message: "This is an Express App!" });
+});
+
+/* GET user page. */
+router.get('/:user', function(req, res, next) {
+  console.log('data: ', data);
+  console.log('user: ', req.params.user);
+  res.render('userpage', data[req.params.user]);
 });
 
 module.exports = router;
